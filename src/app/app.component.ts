ea86dd6aef7f7c2d0bcc6c42cef1afb9cf2965ba
tallyto <rodrigues.tallyto@hotmail.com>
2023-11-15 17:28:01 -0300

    feat: criado menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<app-menu-lateral></app-menu-lateral>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
