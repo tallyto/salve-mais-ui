569ed8d5e0ea37f45f081be683c482ca39c2ce07
tallyto <rodrigues.tallyto@hotmail.com>
2026-05-11 17:06:15 -0300

    Update environment configurations and package lock

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.myfinance.lyto.com.br/api'
-};
+  apiUrl: 'https://salvemais.tallyto.com/api'
+};  
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.myfinance.lyto.com.br/api'
+  apiUrl: 'https://salvemais.tallyto.com/api'
 };
