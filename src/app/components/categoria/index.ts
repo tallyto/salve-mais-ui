f8e182e3e1b8be9584c1f86f34b856b447a5e49a
tallyto <rodrigues.tallyto@hotmail.com>
2026-06-15 16:34:13 -0300

    chore(categoria): remover export deprecado de CategoriaModule
    - Remover export de CategoriaModule do index.ts
    - Manter apenas exports dos componentes standalone

--- src/app/components/categoria/index.ts
+++ src/app/components/categoria/index.ts
@@ -1,6 +1,5 @@
 // Arquivo de compatibilidade - exporta os componentes para uso externo
 export { CategoriaFormComponent } from './categoria-form/categoria-form.component';
-export { CategoriaModule } from './categoria.module';
 export { CategoriaContainerComponent } from './categoria-container/categoria-container.component';
 export { ListCategoriasComponent } from './list-categorias/list-categorias.component';
 export { CategoriaTypeChipComponent } from './categoria-type-chip/categoria-type-chip.component';
